86777a6b0968b95171a9fd96915db6bae8eb2d3e
eventia <eventia@gmail.com>
2018-10-24 18:38:18 +0900

    Add: Gugudan

--- Java2Gugu/src/com/ludens/gugudan/Gugudan.java
+++ Java2Gugu/src/com/ludens/gugudan/Gugudan.java
@@ -1,23 +1,21 @@
 package com.ludens.gugudan;
-
 public class Gugudan {
 
+	// 메소드(함수) 사용
+	public int multiply2numbers(int a, int b) {
+		return a*b;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		int a;
 		int b;
-		
-//		a = 2;
-//		b = 2;
-		
+		// 1단계 - 객체를 만든다.
+		Gugudan gugu = new Gugudan();
 		for(b=2; b<10; b++) {
 			for(a=1; a<10; a++ ) {
-				int c = a * b;
-				System.out.println(b + " * " + a + " = " + c);
+				// 2단계 - 만든 객체안에 있는 메소드를 사용한다.
+				System.out.println(b + " * " + a + " = " + gugu.multiply2numbers(a,b));
 			}
 		}
-
 	}
-
 }
